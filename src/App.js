e4e84cab569e819154a48cb70bce99b82a146ce9
Vitaly Krenel <vitaly.krenel@gmail.com>
2021-06-12 15:25:30 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1 className="Title">Housework Undoer</h1>
+      <h2 className="Subtitle">Task #001</h2>
+      <p className="Description">
+        This content is just a placeholder, feel free to remove it =)
+      </p>
+    </div>
+  );
+};
